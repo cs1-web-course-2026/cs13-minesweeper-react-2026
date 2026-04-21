e36673fc672cc9e6568d8fac0b68599c515f96ab
Shevelia Viktoria <shevelia.viktoriia@student.karazin.ua>
2026-04-21 14:20:17 +0300

    deleting redundant files

--- src/pages/ШевеляВікторія/components/RestartButton.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './RestartButton.module.css'
-
-export default function RestartButton({ onRestart }) {
-  return (
-    <button type="button" className={styles.btn} onClick={onRestart}>
-      Нова гра
-    </button>
-  )
-}
