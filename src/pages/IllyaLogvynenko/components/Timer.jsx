111b60b3bf3b3c9632bbf2d24d71085e0b2925bb
Illya Logvynenko <ilyamyrakami@gmail.com>
2026-04-21 14:04:38 +0300

    Enhance Timer component with aria-label
    Added aria-label for accessibility to Timer component.

--- src/pages/IllyaLogvynenko/components/Timer.jsx
+++ src/pages/IllyaLogvynenko/components/Timer.jsx
@@ -1,7 +1,11 @@
-import styles from '../Minesweeper.module.css';
+import styles from '../Minesweeper.module.css'
 
 function Timer({ value }) {
-  return <div className={styles.counter}>{value}</div>;
+  return (
+    <div className={styles.counter} aria-label={`Time: ${value}`}>
+      {value}
+    </div>
+  )
 }
 
-export default Timer;
+export default Timer
